08451f6a5811429349be205e0be72bf32924c8f5
ritugupta64 <31064674+ritugupta64@users.noreply.github.com>
2020-05-29 12:02:46 +0530

    fetch the data with react hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  // const [state, isState] = useState({results:[]});
+  const [state, isState] = useState([])
+  const baseUrl = "https://rickandmortyapi.com/api/character/";
+
+  const fetchUrl = async () => {
+    await fetch(baseUrl)
+      .then((res) => res.json())
+      // .then((data) => isState(data));
+      .then((data) => isState(data.results))
+  };
+
+  useEffect(() => {
+    fetchUrl();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>test{state.map((item) => item.id)}</span>
     </div>
   );
 }
